96ede3e5d08497e6b59eb2dfeb36709270691c13
Gustavo <gustavocarmanhanis@gmail.com>
2024-10-24 09:09:05 -0300

    corolla

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>Test for router</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Hello There Vue!</h1>
+  </main>
+</template>
